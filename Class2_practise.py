c6e663c430904ba40ff39ea99bf762b08f746cb9
Iamthecoderofgods <22mjames@strichardgwyn.school>
2024-10-22 18:50:25 +0100

    Add files via upload

---
+++ Class2_practise.py
@@ -0,0 +1,4 @@
+About_me = {"hobby":"Gaming", "favorite_subject":"IT", "Name":"Max", "surname":"James", "school":"st_richard_gwyn"}
+print(About_me)
+print(About_me.keys())
+print(About_me.values())
